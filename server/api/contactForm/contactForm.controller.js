1915dc9580bc0756df29912b2b5cef47433b9332
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-10-22 18:16:01 -0200

    almost done

--- server/api/contactForm/contactForm.controller.js
+++ server/api/contactForm/contactForm.controller.js
@@ -65,11 +65,13 @@ function handleError(res, statusCode) {
 }
 
 function handleSendEmail(res, msg) {
-  console.log('no email', process.env);
-  app.mailer.send('email', {
+  app.mailer.send({
+    template: 'email',
+    bcc: 'jrogatis@metaconexao.com.br'
+  }, {
     to: msg.email,
-    subject: 'Your contact for Jean', // REQUIRED.
-    otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables.
+    subject: 'Your contact with Jean', // REQUIRED.
+    message: msg.message
   }, function (err) {
     if (err) {
       // handle error
