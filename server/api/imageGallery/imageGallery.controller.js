c74e488be002e9d9acfe8a6aa59c9b9f5a39e77b
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-10-28 19:12:27 -0200

    new features

--- server/api/imageGallery/imageGallery.controller.js
+++ server/api/imageGallery/imageGallery.controller.js
@@ -20,10 +20,6 @@ exports.index = index;
 exports.show = show;
 exports.destroy = destroy;
 
-var _fastJsonPatch = require('fast-json-patch');
-
-var _fastJsonPatch2 = _interopRequireDefault(_fastJsonPatch);
-
 var _awsSdk = require('aws-sdk');
 
 var _awsSdk2 = _interopRequireDefault(_awsSdk);
@@ -44,11 +40,9 @@ _awsSdk2.default.config.update({
   region: 'us-east-1'
 });
 var s3 = new _awsSdk2.default.S3();
-
 var params = {
   Bucket: 'rogatis'
 };
-
 var s3Url = 'https://s3.amazonaws.com/rogatis';
 
 exports.signing = function (req, res) {
@@ -61,13 +55,13 @@ exports.signing = function (req, res) {
   var expiration = (0, _moment2.default)().add(5, 'm').toDate(); //15 minutes
 
   var s3Policy = {
-    'expiration': expiration,
-    'conditions': [{
-      'bucket': 'rogatis'
+    expiration: expiration,
+    conditions: [{
+      bucket: 'rogatis'
     }, ['starts-with', '$key', path], {
-      'acl': readType
+      acl: readType
     }, {
-      'success_action_status': '201'
+      success_action_status: '201'
     }, ['starts-with', '$Content-Type', request.type], ['content-length-range', 2048, 10485760]]
   };
 
@@ -92,35 +86,36 @@ exports.signing = function (req, res) {
   res.jsonp(credentials);
 };
 
-function respondWithResult(res, statusCode) {
+/*function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
-  return function (entity) {
+  return function(entity) {
     if (entity) {
       return res.status(statusCode).json(entity);
     }
     return null;
   };
-}
+}*/
 
-function removeEntity(res) {
-  return function (entity) {
-    if (entity) {
-      return entity.remove().then(function () {
-        res.status(204).end();
-      });
+/*function removeEntity(res) {
+  return function(entity) {
+    if(entity) {
+      return entity.remove()
+        .then(() => {
+          res.status(204).end();
+        });
     }
   };
-}
+}*/
 
-function handleEntityNotFound(res) {
-  return function (entity) {
-    if (!entity) {
+/*function handleEntityNotFound(res) {
+  return function(entity) {
+    if(!entity) {
       res.status(404).end();
       return null;
     }
     return entity;
   };
-}
+}*/
 
 function handleError(res, statusCode) {
   statusCode = statusCode || 500;
@@ -131,29 +126,28 @@ function handleError(res, statusCode) {
 
 // Gets a list of images
 function index(req, res) {
-
-  console.log(process.env.aws_access_key_id);
   return s3.listObjects(params).promise().then(function (data) {
     var images = [];
     data.Contents.map(function (image) {
-
       images.push(image.Key);
     });
     return res.status(200).json(images);
   }).catch(function (err) {
     console.log(err);
-    handleError(res);
+    return handleError(res);
   });
 }
 
 // Gets a single Posts from the DB
-function show(req, res) {
-  return Posts.findById(req.params.id).exec().then(handleEntityNotFound(res)).then(respondWithResult(res)).catch(handleError(res));
-}
+function show(req, res) {}
+/*return Posts.findById(req.params.id).exec()
+  .then(handleEntityNotFound(res))
+  .then(respondWithResult(res))
+  .catch(handleError(res));*/
+
 
 // Deletes a image from the s3
 function destroy(req, res) {
-  console.log(req.params.id);
   var paramsToDelete = {
     Bucket: 'rogatis',
     Key: req.params.id
@@ -163,7 +157,7 @@ function destroy(req, res) {
     return res.status(200);
   }).catch(function (err) {
     console.log(err);
-    handleError(res);
+    return handleError(res);
   });
 }
 //# sourceMappingURL=imageGallery.controller.js.map
