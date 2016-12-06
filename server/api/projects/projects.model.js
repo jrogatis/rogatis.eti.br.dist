189ebd2c813443bd150b4dc6ff3f35e352aba27e
Jean Philip de Rogatis <jrogatis@iMac-de-Jean.local>
2016-12-06 11:39:25 -0200

    layout

--- server/api/projects/projects.model.js
+++ server/api/projects/projects.model.js
@@ -11,11 +11,12 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var ProjectsSchema = new _mongoose2.default.Schema({
-  title: String,
-  desc: String,
-  imgUrl: String,
-  siteUrl: String,
-  displayFront: Boolean
+  title: { type: 'String', required: true },
+  type: { type: 'String', required: true },
+  desc: { type: 'String', required: true },
+  imgUrl: { type: 'String', required: true },
+  siteUrl: { type: 'String', required: true },
+  displayFront: { type: Boolean, required: true }
 });
 
 exports.default = _mongoose2.default.model('Projects', ProjectsSchema);
